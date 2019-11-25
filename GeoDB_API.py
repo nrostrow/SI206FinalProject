d7fe97f01b9e234d72a1dcdc4778b8516cf70ff2
Noah Ostrowski <nrostrow@umich.edu>
2019-11-25 17:40:38 -0500

    made the actual city codes list

--- GeoDB_API.py
+++ GeoDB_API.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
-#empty for now, you can test by adding 'Q60' (new york city)
+# empty for now, you can test by adding 'Q60' (new york city)
 city_codes = []
+# full city_codes list
+# city_codes = ['Q60', 'Q65', 'Q1297', 'Q16555', 'Q1345', 'Q16556', 'Q975', 'Q16552', 'Q16557', 'Q16553', 'Q16559', 'Q16568', 'Q62', 'Q6346', 'Q16567', 'Q16558', 'Q16565', 'Q5083', 'Q16554', 'Q16562', 'Q12439', 'Q61', 'Q100', 'Q16563', 'Q23197']
 base_url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/"
 headers = {
     'x-rapidapi-host': "wft-geo-db.p.rapidapi.com",
