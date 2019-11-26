8e64ac8c94e1eb73d107459b9b22cf8811e31226
Matt Fannin <mfannin@umich.edu>
2019-11-26 16:40:04 -0500

    Changes

--- Google_API.py
+++ Google_API.py
@@ -1,21 +1,24 @@
 import requests 
 import json
+import re
 
 
 def get_Google_data(data):
 
+    lat_long_list = []
     api_key = 'AIzaSyAg9jsCeHF3VjdMJqEAuT90fpArAXQZld4'
     #API key given from Google
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address={},{},{}&key={}'
     #From Documentation
 
-
+    data = re.sub(r'[^\w\s]','',data)
+    #To get rid of the comma, and to forget the zip code
     data_pieces = data.split()
-    street1 = data_pieces[0:3]
+    street1 = data_pieces[0:-3]
     s = " "
     street = s.join(street1) 
-    city = data_pieces[3]
-    state = data_pieces[4]
+    city = data_pieces[-3]
+    state = data_pieces[-2]
     #Need to split up data, into pieces of address in order to put them in url
 
     request_url = base_url.format(street, city, state, api_key)
@@ -27,14 +30,15 @@ def get_Google_data(data):
     #print(json.dumps(data_dict,indent=4))
     results = data_dict["results"]
     for result in results:
-        print(result["geometry"]['location']["lat"])
-        print(result["geometry"]['location']['lng'])
+        lat = result["geometry"]['location']["lat"]
+        lat_long_list.append(lat)
+        lon = result["geometry"]['location']['lng']
+        lat_long_list.append(lon)
 
-#get_Google_data("644 Selden St","Detroit", "MI")
-#644 Selden St Detroit, MI 48201
+    return lat_long_list
+
 def main():
-    data = "644 Selden St Detroit MI"
-  
+    data = "644 Selden hi St Detroit, MI 48201"
     get_Google_data(data)
 
 if __name__ == "__main__":
