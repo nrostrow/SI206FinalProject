ba6bb0bca8bcd70562e65ffc3361e9b7ec3a35d8
Matt Fannin <mfannin@umich.edu>
2019-12-07 16:04:50 -0500

    google database done

--- Google_API.py
+++ Google_API.py
@@ -13,15 +13,18 @@ def get_Google_data(data):
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address={},{},{}&key={}'
     #From Documentation
 
-    data = re.sub(r'[^\w\s]','',data)
+    #data = re.sub(r'[^\w\s]','',data)
     #To get rid of the comma, and to forget the zip code
-    data_pieces = data.split()
-    street1 = data_pieces[0:-3]
-    s = " "
-    street = s.join(street1) 
-    city = data_pieces[-3]
-    state = data_pieces[-2]
+    #data_pieces = data.split()
+    # street1 = data_pieces[0:-3]
+    # s = " "
+    # street = s.join(street1) 
+    # city = data_pieces[-3]
+    # state = data_pieces[-2]
     #Need to split up data, into pieces of address in order to put them in url
+    street = data[1]
+    city = data[2]
+    state = data[3]
 
     request_url = base_url.format(street, city, state, api_key)
     #Need to pass in these parameters in order to have a complete request
@@ -48,36 +51,46 @@ def setUpDatabase(db_name):
     return cur, conn
 # Function to set up the main database, called final.db
 
-def setUpCityLatDatabase(address, lat_and_long, cur, conn):
-    lat = lat_and_long[0]
-    #lat_li.append(lat)
-    #appends first element of the list lat_and_long
-    lon = lat_and_long[1]
-    #long_li.append(lon)
-    #appends second element of the list lat_and_long
-        
-    cur.execute("INSERT INTO Latitude (Address, Latitude) VALUES (?,?)",(address, lat))
-        #Inserts data into the Latitude table
-    cur.execute("INSERT INTO Longitude (Address, Longitude) VALUES (?,?)",(address, lon))
+def restaurant_list_creator(cur, conn):
+    restaurants = []
+    cur.execute("SELECT restaurant_id, address, city, state FROM YelpAddress")
+    rows = cur.fetchall()
+    for row in rows:
+        restaurants.append((row[0], row[1], row[2], row[3]))
+    return restaurants
+
+def clear_databases(cur, conn):
+    cur.execute("DROP TABLE IF EXISTS Latitude")
+    cur.execute("DROP TABLE IF EXISTS Longitude")
     conn.commit()
-#sets up a database
+
+
+def latDatabase(data, lat_and_long, cur, conn):
+    lat = lat_and_long[0]     
+    cur.execute("INSERT INTO Latitude (restaurant_id, Latitude) VALUES (?,?)",(data[0], lat))
+    conn.commit()
+
+def longDatabase(data, lat_and_long, cur, conn):
+    lon = lat_and_long[1]
+    cur.execute("INSERT INTO Longitude (restaurant_id, Longitude) VALUES (?,?)",(data[0], lon))
+    conn.commit()
 
 def main():
-    cur, conn = setUpDatabase('Lat&Long.db')
+    cur_ll, conn_ll = setUpDatabase('Lat&Long.db')
+    cur_y, conn_y = setUpDatabase('yelp.db')
 
-    cur.execute("CREATE TABLE IF NOT EXISTS Latitude (Address TEXT PRIMARY KEY, Latitude REAL)") 
-    #Creates first table for API, with the address and latitude
+    clear_databases(cur_ll, conn_ll)
 
-    cur.execute("CREATE TABLE IF NOT EXISTS Longitude (Address TEXT PRIMARY KEY, Longitude REAL)") 
-    #Creates second table for API, with the address and longitude
+    restaurants = restaurant_list_creator(cur_y, conn_y)
 
-    address_list = ['12025 Brandywine Drive Brighton, MI', '800 Fuller St AnnArbor, MI']
+    cur_ll.execute("CREATE TABLE IF NOT EXISTS Latitude (restaurant_id TEXT PRIMARY KEY, Latitude REAL)")
+    cur_ll.execute("CREATE TABLE IF NOT EXISTS Longitude (restaurant_id TEXT PRIMARY KEY, Longitude REAL)")
+    conn_ll.commit()
     
-    for i in address_list:
+    for i in restaurants:
         lat_and_long = get_Google_data(i)
-
-        setUpCityLatDatabase(i, lat_and_long, cur, conn)
-
+        latDatabase(i, lat_and_long, cur_ll, conn_ll)
+        longDatabase(i, lat_and_long, cur_ll, conn_ll)
 
 if __name__ == "__main__":
     main()
