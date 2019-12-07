a023eebddf93a1b6507d9308c6ac6f2862edcf4c
sdobrovi <55407425+sdobrovi@users.noreply.github.com>
2019-12-07 15:33:38 -0500

    database change

--- New_yelpfile.py
+++ New_yelpfile.py
@@ -41,6 +41,7 @@ def Yelp_data_populate(data, cur, conn):
     name_list = []
     city_list = []
     address_list = []
+    state_list = []
     zipcode_list = []
     phone_list = []
     rating_list = []
@@ -59,10 +60,12 @@ def Yelp_data_populate(data, cur, conn):
         address =  i['location']
         address_list.append(address['address1'])
 
+        state = i['location']['state']
+        state_list.append(state)
+
         zipcode = i['location']
         zipcode_list.append(zipcode["zip_code"])
         
-
         phone_num = i['phone']
         phone_list.append(phone_num)
 
@@ -74,8 +77,8 @@ def Yelp_data_populate(data, cur, conn):
 
     for i in range(len(city_list)):
         try:
-            cur.execute("INSERT INTO Yelp (restaurant_id ,name, city, phone_num, rating, reviews) VALUES (?,?,?,?,?,?)",(id_list[i],name_list[i],city_list[i],phone_list[i],rating_list[i],reviewcount_list[i]))
-            cur.execute("INSERT INTO YelpAddress (restaurant_id, address, city, zipcode) VALUES (?,?,?,?)",(id_list[i],address_list[i],city_list[i],zipcode_list[i]))
+            cur.execute("INSERT INTO Yelp (restaurant_id ,name, city, phone_num, rating, reviews) VALUES (?,?,?,?,?,?)",(id_list[i],name_list[i],city_list[i], phone_list[i],rating_list[i],reviewcount_list[i]))
+            cur.execute("INSERT INTO YelpAddress (restaurant_id, address, city, zipcode, state) VALUES (?,?,?,?,?)",(id_list[i],address_list[i],city_list[i],zipcode_list[i], state_list[i]))
         except:
             print(name_list[i], city_list[i])
     conn.commit()
@@ -85,7 +88,7 @@ def Yelp_data_setup(cur, conn):
     cur.execute("DROP TABLE IF EXISTS YelpAddress")
     
     cur.execute("CREATE TABLE Yelp (restaurant_id TEXT PRIMARY KEY,name TEXT, city TEXT, phone_num TEXT, rating REAL, reviews INTEGER)")
-    cur.execute("CREATE TABLE YelpAddress(restaurant_id TEXT PRIMARY KEY, address TEXT, city TEXT, zipcode TEXT)")
+    cur.execute("CREATE TABLE YelpAddress(restaurant_id TEXT PRIMARY KEY, address TEXT, city TEXT, zipcode TEXT, state TEXT)")
     conn.commit()
 
 def create_database(cur, conn):
@@ -95,7 +98,7 @@ def create_database(cur, conn):
 cur_yelp, conn_yelp = Database_builder('yelp.db')
 cur_cities, conn_cities = Database_builder('cities.db')
 cities = City_list_creator(cur_cities, conn_cities)
-# create_database(cur_yelp, conn_yelp)
+#create_database(cur_yelp, conn_yelp)
 for city in cities:
     Yelp_data_populate(location(api_key, city), cur_yelp, conn_yelp)
 #Yelp_data_populate(location(api_key), cur_yelp, conn_yelp)
