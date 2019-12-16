94f9d47394f7fb313c838cc3c8231586d2d485dc
Matt Fannin <mfannin@umich.edu>
2019-12-16 17:19:01 -0500

    visualizations

--- PlotVisualizations.py
+++ PlotVisualizations.py
@@ -90,18 +90,18 @@ def plot_average_city_rating(cur, conn):
     plt.savefig("city_ratings.png", bbox_inches='tight')
 
     def plot_lat_long(cur, conn):
-    lat = []
-    lon = []
-    cur.execute("SELECT Latitude, Longitude, Latitude.restaurant_id FROM Latitude INNER JOIN Longitude ON Latitude.restaurant_id = Longitude.restaurant_id")
-    rows = cur.fetchall()
-    for i in rows:
-        lat.append(i[0])
-        lon.append(i[1])
-    plt.scatter(lon,lat, c = 'Black', marker = "x")
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.title('Latitude and Longitude of Restaurants in US Cities')
-    plt.savefig("lat_long.png", bbox_inches = 'tight')
+        lat = []
+        lon = []
+        cur.execute("SELECT Latitude, Longitude, Latitude.restaurant_id FROM Latitude INNER JOIN Longitude ON Latitude.restaurant_id = Longitude.restaurant_id")
+        rows = cur.fetchall()
+        for i in rows:
+            lat.append(i[0])
+            lon.append(i[1])
+        plt.scatter(lon,lat, c = 'Black', marker = "x")
+        plt.xlabel('Longitude')
+        plt.ylabel('Latitude')
+        plt.title('Latitude and Longitude of Restaurants in US Cities')
+        plt.savefig("lat_long.png", bbox_inches = 'tight')
 
 def main():
     cur, conn = connect_db('data.db')
